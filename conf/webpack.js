9eebc3a13e8f0b128ea3966526b9aa50ed4adb40
MatthiasDummler <matthias.dummler@cordaware.com>
2022-01-17 15:11:53 +0100

    Removed: SASS Version: 2.0.1

--- conf/webpack.js
+++ conf/webpack.js
@@ -38,14 +38,6 @@ module.exports = {
 				]
 			},
 			{
-				test: /\.scss$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-			},
-			{
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {
